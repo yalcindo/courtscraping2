34851f96a502ec4b9ed0be3c246ff1f0267723c5
yalcindo <yalcindo1@gmail.com>
2013-12-22 00:53:45 -0700

    Getting each value and fixing comma issuues

--- app.js
+++ app.js
@@ -33,58 +33,62 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+ 
+ var apple="923,864,612";
+  
 writeStream.write('Name,' + 'County,'+'Indictment,'+'Defendant Number,'+'DOB,'+
 	'Age Today,' + 'Offense Description,' + 'Sentencing Place,'+'Sentencing Date,'+'Sentencing Type ,'+'Age at sentence,'+
 	'Jail,' + 'Parole Ineligibility,'+'Probation,'+'Penalty,' + 'Fine,'+'Lab Fee,'+ 'DEDR,'+'Restitution,'+'Judge,' + 'Comments'+'\n');
-
-	request("http://php.app.com/njsent/details.php?recordID=1",function(err,response,body)
+ var getCourtData=function(num){
+  if (1000<num){return;}
+	request("http://php.app.com/njsent/details.php?recordID="+num,function(err,response,body)
 	{
 		if (!err && response.statusCode == 200) {
 			$ =cheerio.load(body);
-			var csvString="";
-			$(".results tr").each(function(i,el)
-			{
+			var tableOrder=$(".results tr td");
 
-				var column1=$(this).children().eq(1).text();
-				var column2=$(this).children().eq(3).text();
-				
-				column1 = column1.replace(";", "");
-				column2 = column2.replace(";", "");
-				column1 = column1.replace(",", "");
-				column2 = column2.replace(",", "");
-				if(i!==0 && i!==4 && i!==6 && i!==10 && i!==13 && i!==5)
-				{
-					if(column1==="")
-					{
-						column1="No information"+i;
-					}
-					if(column2==="")
-					{
-						column2="no information"+i;
-					}
-				}
-			
-				if(column1!=="" && column2!==""){
-					if(csvString==="")
-					{
-						csvString=column1+','+column2;
-					}else{
-						csvString= csvString+','+column1+','+column2 ;
-					}
-				}else if(column1)
-				{
-					csvString=csvString+','+ column1;
-				}else if(column2)
-				{
-					csvString= csvString +','+column2;
-				}
-			});
-			 
-          	writeStream.write(csvString + '\n');
-         
+			var name=tableOrder.eq(1).text();
+			 	name=name.replace(",","");
+			var county=tableOrder.eq(3).text();   
+       		var indicment=tableOrder.eq(5).text();
+       		indicment=indicment.replace(",","");
+       		var defNum =tableOrder.eq(7).text();
+       		var dob =tableOrder.eq(9).text();
+       		var ageToday=tableOrder.eq(11).text();
+       		var offDesc=tableOrder.eq(14).text();
+       		  offDesc=offDesc.replace("\n","");
+       		  offDesc=offDesc.replace(/,/g,"");
+       		var place=tableOrder.eq(17).text();tableOrder.eq(7).text();
+       		var date=tableOrder.eq(19).text();
+       		var type =tableOrder.eq(21).text();
+       		var ageAtSentence=tableOrder.eq(23).text();
+       		var jail=tableOrder.eq(25).text();
+       		var paroleIn=tableOrder.eq(27).text();
+       		var prob=tableOrder.eq(29).text();
+       		var penalty=tableOrder.eq(32).text();
+       		penalty=penalty.replace(/,/g,"");
+       		var fine=tableOrder.eq(34).text();
+       		var labFee=tableOrder.eq(36).text();
+       		var dedr=tableOrder.eq(38).text();
+       			dedr=dedr.replace(",","");
+       		var restituion=tableOrder.eq(40).text();
+       		    restituion=restituion.replace(",","");
+       		var judge=tableOrder.eq(43).text();
+       			judge=judge.replace(",","");
+       		var comments=tableOrder.eq(45).text();
+       		    comments=comments.replace(/;/g,"");
+       		    comments=comments.replace(/,/g,"");
+       		  
+         		console.log(name,county,indicment,defNum,dob,ageToday,
+         	offDesc,place,date,type,ageAtSentence,jail,paroleIn,
+         	prob,penalty,fine,labFee,dedr,restituion,judge,comments);
 		}
+		writeStream.write(name+','+county+','+indicment+','+defNum+','+dob+','+ageToday+','+offDesc+','+place+','+date+','+type+','+ageAtSentence+','+jail+','+paroleIn+','+prob+','+penalty+','+fine+','+labFee+','+dedr+','+restituion+','+judge+','+comments+'\n');
 	});
-
+   num++;
+	getCourtData(num);
+};
+getCourtData(1)
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
